5698ec67c33fd27ebf65cef35bb5e356e7ad2cb7
Safiye <safiye_87abv.bg@icloud.com>
2024-03-08 21:37:45 +0000

    ilk calisma

---
+++ modul_1/my_math.py
@@ -0,0 +1,12 @@
+def my_pow(x,y):
+    
+    """x in y kuvetini alir"""
+    return x ** y
+
+my_pi = 3.14
+my_pi
+def tek_cift(x):
+    
+    """tek sayinin karesini cift sayinin kedisini dondurur"""
+    
+    return x ** 2 if x % 2 == 1 else x
